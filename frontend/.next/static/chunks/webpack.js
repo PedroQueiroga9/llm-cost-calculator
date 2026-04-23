1dd9ade11921f5737c276a062036ab3b83a5dae4
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2026-04-23 11:34:15 -0300

    Router API

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c39d9d8c492ad115"; }
+/******/ 		__webpack_require__.h = function() { return "5038113724c2c30c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
